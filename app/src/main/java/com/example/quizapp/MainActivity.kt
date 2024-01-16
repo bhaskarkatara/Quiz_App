1ae16831f11635211d42e65c350375a8070dc81f
bhaskar katara <bhaskarkatararjo5@gmail.com>
2024-01-16 23:22:09 +0530

    Add Database

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -7,22 +7,53 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var database : FirebaseDatabase
+    private lateinit var  userRef : DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val btnStart : Button = findViewById(R.id.btn_start)
         val etName : EditText = findViewById(R.id.et_name)
+        val etId : EditText = findViewById(R.id.et_id)
+
+        userRef = FirebaseDatabase.getInstance().reference.child("newUser")
 
          btnStart.setOnClickListener {
              if(etName.text.isEmpty()){
                  Toast.makeText(this, "Please enter your name first", Toast.LENGTH_SHORT).show()
              }
+             else if(etId.text.isEmpty()){
+                 Toast.makeText(this, "Please enter your ID ", Toast.LENGTH_SHORT).show()
+             }
              else{
+
+                 val userName = etName.text.toString()
+                 val userId = etId.text.toString()
+
+                 // Create a User object or a Map to store both name and ID
+                 val userMap = HashMap<String, Any>()
+                 userMap["name"] = userName
+                 userMap["id"] = userId
+
+                 // Save user data to Firebase
+//                 userRef.push().setValue(userMap)
+                 userRef.push().setValue(userMap)
+//                     .addOnSuccessListener {
+//                         // Data successfully written
+//                         Toast.makeText(this, "Data Inserted", Toast.LENGTH_SHORT).show()
+//                     }
+//                     .addOnFailureListener { e ->
+//                         // Handle failure
+//                         Toast.makeText(this, "Failed to insert data: $e", Toast.LENGTH_LONG).show()
+//                     }
+                 Toast.makeText(this, "Data Inserted", Toast.LENGTH_SHORT).show()
                  // create intent
                  val intent  = Intent(this,QuizQuestionsActivity :: class.java)
                  // TODO Pass the name through intent using the constant variable which we have created.
@@ -31,7 +62,5 @@ class MainActivity : AppCompatActivity() {
                  finish()
              }
          }
-
-
     }
 }
